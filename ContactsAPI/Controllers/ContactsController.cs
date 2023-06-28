55a0caa7c6b79e11608e85cd0904613a7ce84d07
senia1 <oks314@gmail.com>
2023-06-29 00:27:33 +0300

    add ContactsController.cs and Method GetContacts()

---
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;   
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
+        }
+    }
+}
